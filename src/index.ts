d8526e52886442a178eec4e3ce0c57bf506fae7f
IsaiasPC <isamed92@gmail.com>
2019-12-19 03:44:32 -0300

    observer test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer, observable } from 'rxjs';
+
+const observer: Observer<any> = {
+    next: value => console.log('[next]:', value),
+    error: err => console.warn('[error]:', err),
+    complete: () => console.info('[completed]')
+};
 
 
 // const observable$ =  Observable.create();
@@ -8,6 +14,11 @@ const obs$ = new Observable<string>(subscriber => {
     subscriber.next('hola');
     subscriber.next('mundo');
     subscriber.next('hola');
+    
+
+    //Forced error
+    // const a = undefined;
+    // a.nombre = 'isaias';
 
     subscriber.complete();
     subscriber.next('hola');
@@ -15,4 +26,10 @@ const obs$ = new Observable<string>(subscriber => {
 
 });
 
-obs$.subscribe(resultado => console.log(resultado));
+// obs$.subscribe(
+//     (value) => {console.log('next: ', value);},
+//     (error) => {console.warn('error: ', error);},
+//     () => {console.info('complete');}
+// );
+
+// obs$.subscribe(observer);
