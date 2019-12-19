244968c0dbbe980e9a0f7688b783f821434a5a45
IsaiasPC <isamed92@gmail.com>
2019-12-19 03:47:03 -0300

    clean index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,29 +7,3 @@ const observer: Observer<any> = {
 };
 
 
-// const observable$ =  Observable.create();
-const obs$ = new Observable<string>(subscriber => {
-    subscriber.next('hola');
-    subscriber.next('mundo');
-    subscriber.next('hola');
-    subscriber.next('mundo');
-    subscriber.next('hola');
-    
-
-    //Forced error
-    // const a = undefined;
-    // a.nombre = 'isaias';
-
-    subscriber.complete();
-    subscriber.next('hola');
-    subscriber.next('mundo');
-
-});
-
-// obs$.subscribe(
-//     (value) => {console.log('next: ', value);},
-//     (error) => {console.warn('error: ', error);},
-//     () => {console.info('complete');}
-// );
-
-// obs$.subscribe(observer);
