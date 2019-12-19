a7ef28888d07c92d43a298d2627638fdd478bac6
IsaiasPC <isamed92@gmail.com>
2019-12-19 03:32:18 -0300

    create observable and subscribe on it

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
+import { Observable } from 'rxjs';
 
 
-console.log('Hola Mundo!');
+// const observable$ =  Observable.create();
+const obs$ = new Observable<string>(subscriber => {
+    subscriber.next('hola');
+    subscriber.next('mundo');
+    subscriber.next('hola');
+    subscriber.next('mundo');
+    subscriber.next('hola');
 
+    subscriber.complete();
+    subscriber.next('hola');
+    subscriber.next('mundo');
 
+});
 
-
-
-
-
+obs$.subscribe(resultado => console.log(resultado));
